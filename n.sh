1e1d892d6ea3f78bcdbb19e38e3e86d5db2917f6
wwq0327 <wwq0327@gmail.com>
2012-10-06 11:34:56 -0400

    php

--- n.sh
+++ n.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash 
-#kill -HUP `cat /home/wyatt/nginx/logs/nginx.pid`
-/usr/sbin/nginx -c /home/wyatt/nginx/nginx.conf
+kill -HUP `cat /home/wyatt/nginx/logs/nginx.pid`
+#/usr/sbin/nginx -c /home/wyatt/nginx/nginx.conf
 
